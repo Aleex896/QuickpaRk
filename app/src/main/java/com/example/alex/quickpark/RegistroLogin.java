e98caaea132858eb0c2222d78d943a24fc610e40
Alex Garcia Castro <alexgarciacastro.96@gmail.com>
2017-04-27 09:59:42 +0200

    commit 1

--- app/src/main/java/com/example/alex/quickpark/RegistroLogin.java
+++ app/src/main/java/com/example/alex/quickpark/RegistroLogin.java
@@ -1,7 +1,9 @@
 package com.example.alex.quickpark;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroLogin extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class RegistroLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_login);
+
+        Button botonregistro = (Button) findViewById(R.id.bCrearCuent);
+        botonregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Todo : intent a la otra screen
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View windowScreen = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // ESCONDE BARRA DE NAVEGACION INFERIOR
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        int uiOption2 = View.SYSTEM_UI_FLAG_IMMERSIVE;
+        //windowScreen.setSystemUiVisibility(uiOption2);
+        windowScreen.setSystemUiVisibility(uiOptions);
     }
 }
+
+
