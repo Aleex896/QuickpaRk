7aea06839fc27d969e1e94cb64383080af2a831f
Alex Garcia Castro <alexgarciacastro.96@gmail.com>
2017-04-27 08:44:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alex/quickpark/RegistroLogin.java
@@ -0,0 +1,13 @@
+package com.example.alex.quickpark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_login);
+    }
+}
