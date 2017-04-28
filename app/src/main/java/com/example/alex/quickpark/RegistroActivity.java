a531a119b8591a8a165e009def1eee7cacf163a1
Alex Garcia Castro <alexgarciacastro.96@gmail.com>
2017-04-28 13:13:19 +0200

    commit 1

--- app/src/main/java/com/example/alex/quickpark/RegistroActivity.java
+++ app/src/main/java/com/example/alex/quickpark/RegistroActivity.java
@@ -1,10 +1,13 @@
 package com.example.alex.quickpark;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -13,13 +16,21 @@ public class RegistroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        Button botonregistro = (Button) findViewById(R.id.bVolver);
-        botonregistro.setOnClickListener(new View.OnClickListener() {
+        Button bVolver = (Button) findViewById(R.id.bVolver);
+        TextView tVRegi = (TextView) findViewById(R.id.tVRegi);
+        tVRegi.setTypeface(myFont(this));
+
+        bVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goregistro = new Intent(RegistroActivity.this, FirstScreen.class);
                 startActivity(goregistro);
             }
         });
+    }
+
+    // FUNCION PARA ASIGNAR LA FUENTE
+    public static Typeface myFont(Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "fonts/Walkway SemiBold.ttf");
     }
 }
